1e967c3be8617af9621dac455daa4c663c3416e1
Gabriele Bertoli <gbertoli@cern.ch>
2015-03-16 19:38:26 +0000

    Added functions to plot histograms
    git-svn-id: svn+ssh://svn.cern.ch/reps/atlasinst/Institutes/Stockholm/MonoJetAnalysis/MiniReader_v1/trunk@379947 28d52da6-859c-4cdc-abcc-1ec3d2c02001

---
+++ MiniReader/Root/MiniReaderHistos.cxx
@@ -0,0 +1,39 @@
+#include "MiniReader/MiniReader.h"
+#include <math.h>
+
+#define MINETA -5.
+#define MAXETA 5.
+
+#define SOME_ENUM(DO)       \
+  DO(jetMultiplicity)	    \
+
+#define MAKE_ENUM(VAR) VAR,
+enum HistoNames {
+  SOME_ENUM(MAKE_ENUM)
+};
+
+#define MAKE_STRINGS(VAR) #VAR,
+const char *const HistoNamesString[] = {
+  SOME_ENUM(MAKE_STRINGS)
+};
+
+
+void MiniReaderAlg::DefineHisto(int name, int nbin, float x_low, float x_max)
+{
+  TH1F *temp_pointer = new TH1F(HistoNamesString[name],
+                                HistoNamesString[name], nbin, x_low, x_max);
+
+  m_HistoContainer.push_back(temp_pointer);
+  wk()->addOutput(temp_pointer);
+}
+
+void MiniReaderAlg::InitHisto()
+{
+  // Jet Histos:
+  DefineHisto(jetMultiplicity, 20, 0, 20);
+}
+
+void MiniReaderAlg::FillJets()
+{
+  m_HistoContainer[jetMultiplicity]->Fill(m_jet_mult);
+}
