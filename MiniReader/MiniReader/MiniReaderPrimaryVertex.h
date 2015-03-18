edb7188ec104dd2d4f5527a269b5dc81cede3fb4
Gabriele Bertoli <gbertoli@cern.ch>
2015-03-18 19:03:57 +0000

    Added MiniReaderPrimaryVertex class
    git-svn-id: svn+ssh://svn.cern.ch/reps/atlasinst/Institutes/Stockholm/MonoJetAnalysis/MiniReader_v1/trunk@380161 28d52da6-859c-4cdc-abcc-1ec3d2c02001

---
+++ MiniReader/MiniReader/MiniReaderPrimaryVertex.h
@@ -0,0 +1,31 @@
+#ifndef MINIREADER_MINIREADERPRIMARYVERTEX_H
+#define MINIREADER_MINIREADERPRIMARYVERTEX_H
+
+// ROOT include(s):
+#include <TTree.h>
+
+// STL include(s):
+#include <vector>
+
+class MiniReaderPrimaryVertex {
+
+  friend class MiniReaderAlg;
+
+ private:
+
+  int m_pvtx_n;
+
+  std::vector<int>    *m_pvtx_ntracks;
+  std::vector<double> *m_pvtx_x;
+  std::vector<double> *m_pvtx_y;
+  std::vector<double> *m_pvtx_z;
+  std::vector<double> *m_pvtx_ndof;
+  std::vector<double> *em_pvtx_chi2;
+
+ public:
+
+  MiniReaderPrimaryVertex();
+  void ReadPrimaryVertexBranches(TTree *);
+};
+
+#endif // MINIREADER_MINIREADERPRIMARYVERTEX_H
---
+++ MiniReader/Root/MiniReaderPrimaryVertex.cxx
@@ -0,0 +1,24 @@
+#include "MiniReader/MiniReaderPrimaryVertex.h"
+
+MiniReaderPrimaryVertex::MiniReaderPrimaryVertex()
+{
+  m_pvtx_n = 0;
+
+  m_pvtx_ntracks = 0;
+  m_pvtx_x = 0;
+  m_pvtx_y = 0;
+  m_pvtx_z = 0;
+  m_pvtx_ndof = 0;
+  em_pvtx_chi2 = 0;
+}
+
+void MiniReaderPrimaryVertex::ReadPrimaryVertexBranches(TTree *tree)
+{
+  tree->SetBranchAddress("pvtx_n", &m_pvtx_n);
+  tree->SetBranchAddress("pvtx_ntracks", &m_pvtx_ntracks);
+  tree->SetBranchAddress("pvtx_x", &m_pvtx_x);
+  tree->SetBranchAddress("pvtx_y", &m_pvtx_y);
+  tree->SetBranchAddress("pvtx_z", &m_pvtx_z);
+  tree->SetBranchAddress("pvtx_ndof", &m_pvtx_ndof);
+  tree->SetBranchAddress("epvtx_chi2", &em_pvtx_chi2);
+}
