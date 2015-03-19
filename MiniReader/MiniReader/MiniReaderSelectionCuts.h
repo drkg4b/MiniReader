f705f59e4d3ecc54f5e766a69ca4f9e33a66aa74
Gabriele Bertoli <gbertoli@cern.ch>
2015-03-19 15:39:02 +0000

    Adding MiniReaderSelectionCuts to define cuts
    git-svn-id: svn+ssh://svn.cern.ch/reps/atlasinst/Institutes/Stockholm/MonoJetAnalysis/MiniReader_v1/trunk@380257 28d52da6-859c-4cdc-abcc-1ec3d2c02001

---
+++ MiniReader/MiniReader/MiniReaderSelectionCuts.h
@@ -0,0 +1,28 @@
+#ifndef MINIREADER_MINIREADERSELECTIONCUTS_H
+#define MINIREADER_MINIREADERSELECTIONCUTS_H
+
+namespace M0Cuts {
+
+  static constexpr const float JET_PT = 250000;
+  static constexpr const float MET_CUT = 250000;
+}
+
+namespace M5Cuts {
+
+  static constexpr const float JET_PT = 500000;
+  static constexpr const float MET_CUT = 500000;
+}
+
+namespace M9Cuts {
+
+  static constexpr const float JET_PT = 700000;
+  static constexpr const float MET_CUT = 700000;
+}
+
+namespace EventFilter {
+
+  static constexpr const float DPHI_JETMET = .4;
+  static constexpr const float N_JETS = 3;
+}
+
+#endif // MINIREADER_MINIREADERSELECTIONCUTS_H
