393522363174cc4a989a6da972f1fe1a15170f55
Gabriele Bertoli <gbertoli@cern.ch>
2015-06-25 13:22:34 +0000

    MiniReaderEventFilter.cxx: Moving the MET cut after the PT cut.
    -         m_met.m_EtMissMuVeto > MET_CUT &&
              m_jet.m_jet_pt->at(0) > JET_PT &&
    +         m_met.m_EtMissMuVeto > MET_CUT &&
    
    
    git-svn-id: svn+ssh://svn.cern.ch/reps/atlasinst/Institutes/Stockholm/MonoJetAnalysis/MiniReader_v1/trunk@387050 28d52da6-859c-4cdc-abcc-1ec3d2c02001

--- MiniReader/Root/MiniReaderEventFilter.cxx
+++ MiniReader/Root/MiniReaderEventFilter.cxx
@@ -7,10 +7,10 @@ bool MiniReaderAlg::passEventSelection()
   using namespace EventFilter;
 
   return m_jet.m_jet_mult != 0 &&
-         m_jet.m_jet_isnotbad &&
+    //m_jet.m_jet_isnotbad &&
          m_pvtx.m_pvtx_n > N_PVTX &&
-         m_met.m_EtMissMuVeto > MET_CUT &&
          m_jet.m_jet_pt->at(0) > JET_PT &&
+         m_met.m_EtMissMuVeto > MET_CUT &&
          m_jet.m_jet_mult < N_JETS &&
 	 !isDeltaPhiJetMetLessThanN(DPHI_JETMET) &&
          std::fabs(m_jet.m_jet_eta->at(0)) < JET_ETA;
