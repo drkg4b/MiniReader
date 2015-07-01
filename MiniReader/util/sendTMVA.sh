2a762ac433099070a439d2e5a88180b19c9ace35
Gabriele Bertoli <gbertoli@cern.ch>
2015-07-01 14:38:00 +0000

    sendTMVA.sh: Changed to run on the new signal samples with all backgrounds.
    Added bash arrays to handle multiple backgrounds and corrected the root names
    for the new signal samples.
    
    
    git-svn-id: svn+ssh://svn.cern.ch/reps/atlasinst/Institutes/Stockholm/MonoJetAnalysis/MiniReader_v1/trunk@387485 28d52da6-859c-4cdc-abcc-1ec3d2c02001

--- MiniReader/util/sendTMVA.sh
+++ MiniReader/util/sendTMVA.sh
@@ -20,14 +20,25 @@ fi
 # all the files descriptors thus the < /dev/null (never wait for any input) >
 # out.log and 2>&1 (redirect the stderr to stdout).
 
-# Compressed spectra:
-runSensitivityTMVA ../outDir-TMVA/hist-Compressed_450_435.root ../outDir-TMVA/hist-all_bkg.root n_jet30 < /dev/null > Compressed_450_435_n_jet30.log 2>&1 &
-runSensitivityTMVA ../outDir-TMVA/hist-Compressed_450_435.root ../outDir-TMVA/hist-all_bkg.root n_jet40 < /dev/null > Compressed_450_435_n_jet40.log 2>&1 &
-runSensitivityTMVA ../outDir-TMVA/hist-Compressed_450_435.root ../outDir-TMVA/hist-all_bkg.root n_jet50 < /dev/null > Compressed_450_435_n_jet50.log 2>&1 &
+bkg_samples=()
 
-runSensitivityTMVA ../outDir-TMVA/hist-Compressed_450_425.root ../outDir-TMVA/hist-all_bkg.root n_jet30 < /dev/null > Compressed_450_425_n_jet30.log 2>&1 &
-runSensitivityTMVA ../outDir-TMVA/hist-Compressed_450_425.root ../outDir-TMVA/hist-all_bkg.root n_jet40 < /dev/null > Compressed_450_425_n_jet40.log 2>&1 &
-runSensitivityTMVA ../outDir-TMVA/hist-Compressed_450_425.root ../outDir-TMVA/hist-all_bkg.root n_jet50 < /dev/null > Compressed_450_425_n_jet50.log 2>&1 &
+bkg_samples+=(../outDir-TMVA/hist-ttbarSamples.root)
+bkg_samples+=(../outDir-TMVA/hist-WenuSamples.root)
+bkg_samples+=(../outDir-TMVA/hist-WmunuSamples.root)
+bkg_samples+=(../outDir-TMVA/hist-WtaunuSamples.root)
+bkg_samples+=(../outDir-TMVA/hist-ZeeSamples.root)
+bkg_samples+=(../outDir-TMVA/hist-ZmumuSamples.root)
+bkg_samples+=(../outDir-TMVA/hist-ZnunuSamples.root)
+bkg_samples+=(../outDir-TMVA/hist-ZtautauSamples.root)
+
+# Compressed spectra:
+runSensitivityTMVA ../outDir-TMVA/hist-Compressed_450_435.root ${bkg_samples[@]} n_jet30 < /dev/null > Compressed_450_435_n_jet30.log 2>&1 &
+runSensitivityTMVA ../outDir-TMVA/hist-Compressed_450_435.root ${bkg_samples[@]} n_jet40 < /dev/null > Compressed_450_435_n_jet40.log 2>&1 &
+runSensitivityTMVA ../outDir-TMVA/hist-Compressed_450_435.root ${bkg_samples[@]} n_jet50 < /dev/null > Compressed_450_435_n_jet50.log 2>&1 &
+
+runSensitivityTMVA ../outDir-TMVA/hist-Compressed_450_425.root ${bkg_samples[@]} n_jet30 < /dev/null > Compressed_450_425_n_jet30.log 2>&1 &
+runSensitivityTMVA ../outDir-TMVA/hist-Compressed_450_425.root ${bkg_samples[@]} n_jet40 < /dev/null > Compressed_450_425_n_jet40.log 2>&1 &
+runSensitivityTMVA ../outDir-TMVA/hist-Compressed_450_425.root ${bkg_samples[@]} n_jet50 < /dev/null > Compressed_450_425_n_jet50.log 2>&1 &
 
 # D5
 # runSensitivityTMVA ../outDir-TMVA/hist-D5.root ../outDir-TMVA/hist-all_bkg.root n_jet30 < /dev/null > D5_n_jet30.log 2>&1 &
