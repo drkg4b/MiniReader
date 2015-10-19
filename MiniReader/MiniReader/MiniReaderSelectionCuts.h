ec5eaca0228849c25f1fd3698d665a86bbe7e47d
Gabriele Bertoli <gbertoli@cern.ch>
2015-10-19 16:17:51 +0000

    MiniReaderSelectionCuts.h: Using cuts for JVT studies.
    git-svn-id: svn+ssh://svn.cern.ch/reps/atlasinst/Institutes/Stockholm/MonoJetAnalysis/MiniReader_v1/trunk@395416 28d52da6-859c-4cdc-abcc-1ec3d2c02001

--- MiniReader/MiniReader/MiniReaderSelectionCuts.h
+++ MiniReader/MiniReader/MiniReaderSelectionCuts.h
@@ -21,19 +21,19 @@ namespace M9Cuts {
 
 namespace ZnunuCuts {
 
-  static constexpr const int N_JETS = 999; // WARNING this should be 5
+  static constexpr const int N_JETS = 5; // WARNING this should be 5
   static constexpr const float DPHI_JETMET = .4;
   static constexpr const float MET_HARD = 250000.;
-  static constexpr const float JET_PT_HARD = 250000.; // Changed from 400000 to study asymmetric cuts
+  static constexpr const float JET_PT_HARD = 150000.; // Changed from 400000 to study asymmetric cuts
 }
 
 namespace EventFilter {
 
   static constexpr const float DPHI_JETMET = .4;
-  static constexpr const float N_JETS = 999; // WARNING!!!! should be 5
+  static constexpr const float N_JETS = 5; // WARNING!!!! should be 5
   static constexpr const int N_PVTX = 0;
   static constexpr const float MET_CUT = 250000;
-  static constexpr const float JET_PT = 250000;
+  static constexpr const float JET_PT = 100000;
   static constexpr const float JET_ETA = 2.8;
 }
 
@@ -41,6 +41,7 @@ namespace GoodJetCuts {
 
   static constexpr const float PT_CUT = 30000;
   static constexpr const float ETA_CUT = 2.8;
+  static constexpr const float JVT_CUT = .64;
 }
 
 #endif // MINIREADER_MINIREADERSELECTIONCUTS_H
