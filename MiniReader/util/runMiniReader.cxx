179392f24d5c33f0ea7bc6c14aba2e5f7835c3ce
Gabriele Bertoli <gbertoli@cern.ch>
2015-10-19 16:17:05 +0000

    runMiniReader.cxx: Updated file list to use v105.
    git-svn-id: svn+ssh://svn.cern.ch/reps/atlasinst/Institutes/Stockholm/MonoJetAnalysis/MiniReader_v1/trunk@395409 28d52da6-859c-4cdc-abcc-1ec3d2c02001

--- MiniReader/util/runMiniReader.cxx
+++ MiniReader/util/runMiniReader.cxx
@@ -14,9 +14,12 @@
 
 // STL include(s):
 #include <chrono>
+#include <fstream>
 
 // ROOT Include(s):
 #include "TChain.h"
+
+void ReadChainFromTextFile(TChain &, std::string);
 
 int main(int argc, char *argv[])
 {
@@ -35,74 +38,230 @@ int main(int argc, char *argv[])
    //   .scan(sh, "/home/drkg4b/work/input_samples/BP-v1/BP-01/");
 
    // Background samples:
-   TChain chain_znunu("MiniTree");
+   std::string base_dir = "/home/g/gbertoli/pfs/data/minitrees/v105/";
 
-   chain_znunu.Add("/home/drkg4b/work/input_samples/background/Znunu/*hist-output.root");
+   TChain chain_znunu("MiniTree_NoSys");
 
-   TChain chain_zmumu("MiniTree");
+   ReadChainFromTextFile(chain_znunu, base_dir + "znunu/znunu_file_list.txt");
 
-   chain_zmumu.Add("/home/drkg4b/work/input_samples/background/Zmumu/*hist-output.root");
+   TChain chain_zmumu("MiniTree_NoSys");
 
-   TChain chain_zee("MiniTree");
+   ReadChainFromTextFile(chain_zmumu, base_dir + "zmumu/zmumu_file_list.txt");
 
-   chain_zee.Add("/home/drkg4b/work/input_samples/background/Zee/*hist-output.root");
+   TChain chain_zee("MiniTree_NoSys");
 
-   TChain chain_ztautau("MiniTree");
+   ReadChainFromTextFile(chain_zee, base_dir + "zee/zee_file_list.txt");
 
-   chain_ztautau.Add("/home/drkg4b/work/input_samples/background/Ztautau/*hist-output.root");
+   TChain chain_ztautau("MiniTree_NoSys");
 
-   TChain chain_wmunu("MiniTree");
+   ReadChainFromTextFile(chain_ztautau, base_dir + "ztautau/ztautau_file_list.txt");
 
-   chain_wmunu.Add("/home/drkg4b/work/input_samples/background/Wmunu/*hist-output.root");
+   TChain chain_wmunu("MiniTree_NoSys");
 
-   TChain chain_wenu("MiniTree");
+   ReadChainFromTextFile(chain_wmunu, base_dir + "wmunu/wmunu_file_list.txt");
 
-   chain_wenu.Add("/home/drkg4b/work/input_samples/background/Wenu/*hist-output.root");
+   TChain chain_wenu("MiniTree_NoSys");
 
-   TChain chain_wtaunu("MiniTree");
+   ReadChainFromTextFile(chain_wenu, base_dir + "wenu/wenu_file_list.txt");
 
-   chain_wtaunu.Add("/home/drkg4b/work/input_samples/background/Wtaunu/*hist-output.root");
+   TChain chain_wtaunu("MiniTree_NoSys");
 
-   TChain chain_ttbar("MiniTree");
+   ReadChainFromTextFile(chain_wtaunu, base_dir + "wtaunu/wtaunu_file_list.txt");
 
-   chain_ttbar.Add("/home/drkg4b/work/input_samples/background/ttbar/*hist-output.root");
+   TChain chain_top("MiniTree_NoSys");
+
+   ReadChainFromTextFile(chain_top, base_dir + "top/top_file_list.txt");
+
+   TChain chain_stop("MiniTree_NoSys");
+
+   ReadChainFromTextFile(chain_stop, base_dir + "stop/stop_file_list.txt");
+
+   TChain chain_qcd("MiniTree_NoSys");
+
+   ReadChainFromTextFile(chain_qcd, base_dir + "qcd/qcd_file_list.txt");
+
+   TChain chain_diboson("MiniTree_NoSys");
+
+   ReadChainFromTextFile(chain_diboson, base_dir + "diboson/diboson_file_list.txt");
 
    // Signal samples:
-   TChain chain_d5("MiniTree");
+   TChain chain_d5("MiniTree_NoSys");
 
-   chain_d5.Add("/home/drkg4b/work/input_samples/signals/user.cclement.t02.mc14_13TeV.191040.MadGraphPythia_AUET2BMSTW2008LO_D5_400_1000_MET100_hist-output.root.19926114/*.root");
+   chain_d5.Add((base_dir + "add_303499/"
+		 "user.cclement.ntupsthlm_v105a.mc15_13TeV.303499.Pythia8EvtGen_A14NNPDF23LO_ADDGraviton_d5MD3200_Jet100_hist"
+		 "/*hist-output.root").c_str());
 
-   TChain chain_comp_450_435("MiniTree_NoSys");
+   TChain chain_comp_400_375("MiniTree_NoSys");
 
-   chain_comp_450_435.Add("/home/drkg4b/work/input_samples/signals/compressed/"
-			  "MERGE.mc15.371858.madgraphpythia8evtgen_a14nnpdf23lo_ss_direct_450_435_met100.mc15.hist-output.root/*.root");
+   chain_comp_400_375.Add((base_dir + "compressed/"
+			   "user.cclement.ntupsthlm_v105a.mc15_13TeV.371864.MadGraphPythia8EvtGen_A14NNPDF23LO_SS_direct_400_375_MET100_hist"
+			   "/*hist-output.root").c_str());
+
+   TChain chain_comp_400_385("MiniTree_NoSys");
+
+   chain_comp_400_385.Add((base_dir + "compressed/"
+			   "user.cclement.ntupsthlm_v105a.mc15_13TeV.371857.MadGraphPythia8EvtGen_A14NNPDF23LO_SS_direct_400_385_MET100_hist"
+			   "/*hist-output.root").c_str());
+
+   TChain chain_comp_400_395("MiniTree_NoSys");
+
+   chain_comp_400_395.Add((base_dir + "compressed/"
+			   "user.cclement.ntupsthlm_v105a.mc15_13TeV.371850.MadGraphPythia8EvtGen_A14NNPDF23LO_SS_direct_400_395_MET100_hist"
+			   "/*hist-output.root").c_str());
 
    TChain chain_comp_450_425("MiniTree_NoSys");
 
-   chain_comp_450_425.Add("/home/drkg4b/work/input_samples/signals/compressed/"
-			  "MERGE.mc15.371865.madgraphpythia8evtgen_a14nnpdf23lo_ss_direct_450_425_met100.mc15.hist-output.root/*.root");
+   chain_comp_450_425.Add((base_dir + "compressed/"
+			   "user.cclement.ntupsthlm_v105a.mc15_13TeV.371865.MadGraphPythia8EvtGen_A14NNPDF23LO_SS_direct_450_425_MET100_hist"
+			   "/*hist-output.root").c_str());
+
+   TChain chain_comp_450_435("MiniTree_NoSys");
+
+   chain_comp_450_435.Add((base_dir + "compressed/"
+			   "user.cclement.ntupsthlm_v105a.mc15_13TeV.371858.MadGraphPythia8EvtGen_A14NNPDF23LO_SS_direct_450_435_MET100_hist"
+			   "/*hist-output.root").c_str());
+
+   TChain chain_comp_450_445("MiniTree_NoSys");
+
+   chain_comp_450_445.Add((base_dir + "compressed/"
+			   "user.cclement.ntupsthlm_v105a.mc15_13TeV.371851.MadGraphPythia8EvtGen_A14NNPDF23LO_SS_direct_450_445_MET100_hist"
+			   "/*hist-output.root").c_str());
+
+   TChain chain_comp_500_475("MiniTree_NoSys");
+
+   chain_comp_500_475.Add((base_dir + "compressed/"
+			   "user.cclement.ntupsthlm_v105a.mc15_13TeV.371866.MadGraphPythia8EvtGen_A14NNPDF23LO_SS_direct_500_475_MET100_hist"
+			   "/*hist-output.root").c_str());
+
+   TChain chain_comp_500_485("MiniTree_NoSys");
+
+   chain_comp_500_485.Add((base_dir + "compressed/"
+			   "user.cclement.ntupsthlm_v105a.mc15_13TeV.371859.MadGraphPythia8EvtGen_A14NNPDF23LO_SS_direct_500_485_MET100_hist"
+			   "/*hist-output.root").c_str());
+
+   TChain chain_comp_500_495("MiniTree_NoSys");
+
+   chain_comp_500_495.Add((base_dir + "compressed/"
+			   "user.cclement.ntupsthlm_v105a.mc15_13TeV.371852.MadGraphPythia8EvtGen_A14NNPDF23LO_SS_direct_500_495_MET100_hist"
+			   "/*hist-output.root").c_str());
+
+   TChain chain_comp_550_525("MiniTree_NoSys");
+
+   chain_comp_550_525.Add((base_dir + "compressed/"
+			   "user.cclement.ntupsthlm_v105a.mc15_13TeV.371867.MadGraphPythia8EvtGen_A14NNPDF23LO_SS_direct_550_525_MET100_hist"
+			   "/*hist-output.root").c_str());
+
+   TChain chain_comp_550_535("MiniTree_NoSys");
+
+   chain_comp_550_535.Add((base_dir + "compressed/"
+			   "user.cclement.ntupsthlm_v105a.mc15_13TeV.371860.MadGraphPythia8EvtGen_A14NNPDF23LO_SS_direct_550_535_MET100_hist"
+			   "/*hist-output.root").c_str());
+
+   TChain chain_comp_550_545("MiniTree_NoSys");
+
+   chain_comp_550_545.Add((base_dir + "compressed/"
+			   "user.cclement.ntupsthlm_v105a.mc15_13TeV.371853.MadGraphPythia8EvtGen_A14NNPDF23LO_SS_direct_550_545_MET100_hist"
+			   "/*hist-output.root").c_str());
+
+   TChain chain_comp_600_575("MiniTree_NoSys");
+
+   chain_comp_600_575.Add((base_dir + "compressed/"
+			   "user.cclement.ntupsthlm_v105a.mc15_13TeV.371868.MadGraphPythia8EvtGen_A14NNPDF23LO_SS_direct_600_575_MET100_hist"
+			   "/*hist-output.root").c_str());
+
+   TChain chain_comp_600_585("MiniTree_NoSys");
+
+   chain_comp_600_585.Add((base_dir + "compressed/"
+			   "user.cclement.ntupsthlm_v105a.mc15_13TeV.371861.MadGraphPythia8EvtGen_A14NNPDF23LO_SS_direct_600_585_MET100_hist"
+			   "/*hist-output.root").c_str());
+
+   TChain chain_comp_600_595("MiniTree_NoSys");
+
+   chain_comp_600_595.Add((base_dir + "compressed/"
+			   "user.cclement.ntupsthlm_v105a.mc15_13TeV.371854.MadGraphPythia8EvtGen_A14NNPDF23LO_SS_direct_600_595_MET100_hist"
+			   "/*hist-output.root").c_str());
+
+   TChain chain_comp_650_625("MiniTree_NoSys");
+
+   chain_comp_650_625.Add((base_dir + "compressed/"
+			   "user.cclement.ntupsthlm_v105a.mc15_13TeV.371869.MadGraphPythia8EvtGen_A14NNPDF23LO_SS_direct_650_625_MET100_hist"
+			   "/*hist-output.root").c_str());
+
+   TChain chain_comp_650_635("MiniTree_NoSys");
+
+   chain_comp_650_635.Add((base_dir + "compressed/"
+			   "user.cclement.ntupsthlm_v105a.mc15_13TeV.371862.MadGraphPythia8EvtGen_A14NNPDF23LO_SS_direct_650_635_MET100_hist"
+			   "/*hist-output.root").c_str());
+
+   TChain chain_comp_650_645("MiniTree_NoSys");
+
+   chain_comp_650_645.Add((base_dir + "compressed/"
+			   "user.cclement.ntupsthlm_v105a.mc15_13TeV.371855.MadGraphPythia8EvtGen_A14NNPDF23LO_SS_direct_650_645_MET100_hist"
+			   "/*hist-output.root").c_str());
+
+   TChain chain_comp_700_675("MiniTree_NoSys");
+
+   chain_comp_700_675.Add((base_dir + "compressed/"
+			   "user.cclement.ntupsthlm_v105a.mc15_13TeV.371870.MadGraphPythia8EvtGen_A14NNPDF23LO_SS_direct_700_675_MET100_hist"
+			   "/*hist-output.root").c_str());
+
+   TChain chain_comp_700_685("MiniTree_NoSys");
+
+   chain_comp_700_685.Add((base_dir + "compressed/"
+			   "user.cclement.ntupsthlm_v105a.mc15_13TeV.371863.MadGraphPythia8EvtGen_A14NNPDF23LO_SS_direct_700_685_MET100_hist"
+			   "/*hist-output.root").c_str());
+
+   TChain chain_comp_700_695("MiniTree_NoSys");
+
+   chain_comp_700_695.Add((base_dir + "compressed/"
+			   "user.cclement.ntupsthlm_v105a.mc15_13TeV.371856.MadGraphPythia8EvtGen_A14NNPDF23LO_SS_direct_700_695_MET100_hist"
+			   "/*hist-output.root").c_str());
 
    // CutFlow samples:
-   TChain chain_cutflow("MiniTree_NoSys");
+   // TChain chain_cutflow("MiniTree_NoSys");
 
-   chain_cutflow.Add("/home/drkg4b/work/input_samples/cutFlow_p1872/*.root");
+   // chain_cutflow.Add("/home/drkg4b/work/input_samples/cutFlow_p1872/*.root");
 
 
    // Construct the samples to run on:
    SH::SampleHandler sh;
 
    sh.add(SH::makeFromTChain("ZnunuSamples", chain_znunu));
-   sh.add(SH::makeFromTChain("ZmumuSamples", chain_zmumu));
-   sh.add(SH::makeFromTChain("ZeeSamples", chain_zee));
-   sh.add(SH::makeFromTChain("ZtautauSamples", chain_ztautau));
-   sh.add(SH::makeFromTChain("WmunuSamples", chain_wmunu));
-   sh.add(SH::makeFromTChain("WenuSamples", chain_wenu));
-   sh.add(SH::makeFromTChain("WtaunuSamples", chain_wtaunu));
-   sh.add(SH::makeFromTChain("ttbarSamples", chain_ttbar));
-   // sh.add(SH::makeFromTChain("D5", chain_d5));
-   sh.add(SH::makeFromTChain("Compressed_450_435", chain_comp_450_435));
+   // sh.add(SH::makeFromTChain("ZmumuSamples", chain_zmumu));
+   // sh.add(SH::makeFromTChain("ZeeSamples", chain_zee));
+   // sh.add(SH::makeFromTChain("ZtautauSamples", chain_ztautau));
+   // sh.add(SH::makeFromTChain("WmunuSamples", chain_wmunu));
+   // sh.add(SH::makeFromTChain("WenuSamples", chain_wenu));
+   // sh.add(SH::makeFromTChain("WtaunuSamples", chain_wtaunu));
+   // sh.add(SH::makeFromTChain("TopSamples", chain_top));
+   // sh.add(SH::makeFromTChain("StopSamples", chain_stop));
+   // sh.add(SH::makeFromTChain("QcdSamples", chain_qcd));
+   // sh.add(SH::makeFromTChain("DibosonSamples", chain_diboson));
+
+   sh.add(SH::makeFromTChain("D5", chain_d5));
+
+   // sh.add(SH::makeFromTChain("Compressed_400_375", chain_comp_400_375));
+   // sh.add(SH::makeFromTChain("Compressed_400_385", chain_comp_400_385));
+   // sh.add(SH::makeFromTChain("Compressed_400_395", chain_comp_400_395));
    sh.add(SH::makeFromTChain("Compressed_450_425", chain_comp_450_425));
-   // sh.add(SH::makeFromTChain("ttbarJVTSamples", chain5));
+   sh.add(SH::makeFromTChain("Compressed_450_435", chain_comp_450_435));
+   sh.add(SH::makeFromTChain("Compressed_450_445", chain_comp_450_445));
+   // sh.add(SH::makeFromTChain("Compressed_500_475", chain_comp_500_475));
+   // sh.add(SH::makeFromTChain("Compressed_500_485", chain_comp_500_485));
+   // sh.add(SH::makeFromTChain("Compressed_500_495", chain_comp_500_495));
+   // sh.add(SH::makeFromTChain("Compressed_550_525", chain_comp_550_525));
+   // sh.add(SH::makeFromTChain("Compressed_550_535", chain_comp_550_535));
+   // sh.add(SH::makeFromTChain("Compressed_550_545", chain_comp_550_545));
+   // sh.add(SH::makeFromTChain("Compressed_600_575", chain_comp_600_575));
+   // sh.add(SH::makeFromTChain("Compressed_600_585", chain_comp_600_585));
+   // sh.add(SH::makeFromTChain("Compressed_600_595", chain_comp_600_595));
+   // sh.add(SH::makeFromTChain("Compressed_650_625", chain_comp_650_625));
+   // sh.add(SH::makeFromTChain("Compressed_650_635", chain_comp_650_635));
+   // sh.add(SH::makeFromTChain("Compressed_650_645", chain_comp_650_645));
+   // sh.add(SH::makeFromTChain("Compressed_700_675", chain_comp_700_675));
+   // sh.add(SH::makeFromTChain("Compressed_700_685", chain_comp_700_685));
+   // sh.add(SH::makeFromTChain("Compressed_700_695", chain_comp_700_695));
+
    // sh.add(SH::makeFromTChain("cutFlowSamples", chain_cutflow));
 
    // print what we found:
@@ -148,4 +307,22 @@ int main(int argc, char *argv[])
              << "elapsed time: " << elapsed_seconds.count() << "s\n";
 
    return 0;
+}
+
+void ReadChainFromTextFile(TChain &ch, std::string file_name)
+{
+  std::ifstream in_file(file_name);
+
+  if (!in_file.is_open())
+
+    std::cout<<"Sorry, inputfilenamestream could not be opened!" << std::endl;
+
+  else{
+
+    std::string rootfile;
+
+    while (getline(in_file, rootfile))
+
+      ch.Add(rootfile.c_str());
+  }
 }
